42dd6d17a2eea3cc278fa75aea97596f4587c049
Hıdır Eren Gazioğlu <103429971+Hdreren@users.noreply.github.com>
2023-09-20 13:23:27 +0300

    Add files via upload

---
+++ Image Blur.py
@@ -0,0 +1,20 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+import warnings
+warnings.filterwarnings("ignore")
+
+#Blurring
+img = cv2.imread("new.york.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.figure(), plt.imshow(img), plt.axis("off"), plt.title("original"), plt.show()
+
+#Average Blurring Method
+dst2 = cv2.blur(img, ksize = (3,3))
+plt.figure(), plt.imshow(dst2), plt.axis("off"), plt.title("avarage blur"), plt.show()
+
+
+
+
+
